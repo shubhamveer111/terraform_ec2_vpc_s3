df9ca8c7ed6aa1c46f7b930bd900c49d9b1fe61e
Shubham <shubhamveer111@gmail.com>
2023-02-13 14:51:16 +0530

    Added terrform data

--- backend.tf
+++ backend.tf
@@ -1,20 +1,10 @@
- provider "aws" {
 
-    access_key = "AKIASDEFVBW3VRA4CKUY"
-
-    secret_key = "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"
-
-    region = "us-east-1"
-
-}
- 
- 
- 
- 
  terraform {
   backend "s3" {
     bucket         = "terraformbucketveer"
     key            = "terraform.tfstate"
     region         = "us-east-1" 
+    access_key = "AKIASDEFVBW3VRA4CKUY"
+    secret_key = "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"
   }
-}    
+}  
